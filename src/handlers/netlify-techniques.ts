2b9807d67c679ade6f6200f7ab4a970c6f7e630d
mottox2 <y.mottox2@gmail.com>
2019-02-27 02:00:56 +0900

    Commit post md

--- src/handlers/netlify-techniques.ts
+++ src/handlers/netlify-techniques.ts
@@ -1,3 +1,24 @@
-export default (post: EsaPost) => {
+import Octokit from '@octokit/rest'
 
+const handler = async (post: EsaPost) => {
+  const octokit = new Octokit()
+  octokit.authenticate({
+    type: 'oauth',
+    token: process.env.GITHUB_TOKEN,
+  })
+
+
+  const content = (new Buffer(post.body_md)).toString('base64')
+
+  const res = await octokit.repos.createFile({
+    owner: 'mottox2',
+    repo: 'netlify-techniques',
+    path: `/articles/${post.full_name.replace(/^NetlifyTecniques/, '')}`,
+    message: 'Commit from mottox2/esa-handler',
+    content,
+  })
+
+  console.log(res)
 }
+
+export default handler
